02f6373db3819c88c0474b6aec17d82132c19566
Lucas Michael Mallen <mallenl@eceprog2.ecn.purdue.edu>
2025-04-03 17:41:41 -0400

    first attempt

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,97 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+	struct list_node *node = NULL;
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+	node = malloc(sizeof(struct list_node));
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+	node->value = value;
+	node->next = NULL;
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+	return node;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+void insert_at_head(struct linked_list *list, size_t value) 
+{
+	struct list_node * node = new_node(value);
 
-void free_list(struct linked_list list) {}
+	node->next = list->head;
+
+	list->head = node;
+
+	return;
+}
+
+void insert_at_tail(struct linked_list *list, size_t value) {
+
+	struct list_node * node = new_node(value);
+
+	struct list_node * testNode = list->head;
+
+	while(testNode->next != NULL)
+	{
+		testNode = testNode->next;
+	}
+
+	testNode->next = node;
+
+	return;
+}
+
+size_t remove_from_head(struct linked_list *list) { 
+	
+	struct list_node * node = list->head;
+
+	size_t num = node->value;
+
+	list->head = list->head->next;
+	
+	free(node);
+
+	return num;
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+
+	struct list_node * testNode = list->head;
+	size_t num;
+
+
+	if(testNode == NULL)
+	{
+		return 0;
+	}
+
+	if(testNode->next == NULL)
+	{
+		list->head = NULL;
+		free(testNode);
+		return 0;
+	}
+
+	while(testNode->next->next != NULL)
+	{
+		testNode = testNode->next;
+	}
+
+	num = testNode->next->value;
+	free(testNode->next);
+
+
+	return num; 
+}
+
+void free_list(struct linked_list list) {
+	struct list_node * node;
+
+	while(list.head != NULL)
+	{
+		node = list.head;
+		list.head = list.head->next;
+		free(node);
+	}
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,94 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+	insert_at_tail(&(q->data), serialize(state));
+	return;
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+	
+	return deserialize(remove_from_head(&(q->data)));
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) {
+
+	struct queue q;
+	struct linked_list l;
+
+	l.head = NULL;
+
+	
+
+	q.data = l;
+
+	enqueue(&q, start);
+
+	struct game_state game;
+	struct game_state newGame;
+	
+
+	int col = 0;
+	int row = 0;
+	int flag = 0;
+
+	do
+	{
+		game = dequeue(&q);
+		col = 0;
+		row = 0;
+		flag = 0;
+
+		for(int i = 1; i < 17; i++)
+		{
+			col++;
+			if(col == 4)
+			{
+				col = 0;
+				row++;
+			}
+
+			if((int) game.tiles[row][col] != i % 16)
+			{
+				flag = 1;
+			}
+		}
+
+		if(flag == 0)
+		{
+			if((int) game.empty_row != 0)
+			{
+				newGame = deserialize(serialize(game));
+				move_up(&newGame);
+				enqueue(&q, newGame);
+			}
+
+			if((int) game.empty_row != 3)
+			{
+				newGame = deserialize(serialize(game));
+				move_down(&newGame);
+				enqueue(&q, newGame);
+
+			}
+
+			if((int) game.empty_col != 0)
+			{
+				newGame = deserialize(serialize(game));
+				move_left(&newGame);
+				enqueue(&q, newGame);
+			}
+
+			if((int) game.empty_row != 3)
+			{
+				newGame = deserialize(serialize(game));
+				move_right(&newGame);
+				enqueue(&q, newGame);
+			}
+
+		}
+
+	}
+	while(flag);
+
+	return game.num_steps; 
+}
